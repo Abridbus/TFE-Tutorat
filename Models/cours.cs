819701e9a8ef4e5e0cf027a7b3d5c9abddb3e89d
Abridbus <baptiste_vergote@hotmail.com>
2017-03-03 13:57:58 +0100

    Push before updating Register
    (model/Controller/View)

--- Models/cours.cs
+++ Models/cours.cs
@@ -46,6 +46,5 @@ namespace Tutorat.Models
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<etudiant> etudiant { get; set; }
-
     }
 }
